0de3396df0dde6e667ed9a812ae4988d39f07744
samishken <samishken@gmail.com>
2019-10-22 19:26:44 -0400

    python first training

---
+++ strings.py
@@ -0,0 +1,59 @@
+#Strings
+
+#replace
+phrase = "Giraffe Academy"
+print(phrase.replace("Giraffe", "Elephant"))
+
+
+#index of "Acad" = 8
+phrase = "Giraffe Academy"
+print(phrase.index("Acad"))
+
+
+#index of
+phrase = "Giraffe Academy"
+print(phrase.index("G"))
+
+
+#find the first character
+phrase = "Giraffe Academy"
+print(phrase[0])
+
+
+#how many character inside
+phrase = "Giraffe Academy"
+print(len(phrase))
+
+
+#convert to upper and then check for true or false
+phrase = "Giraffe Academy"
+print(phrase.upper().isupper())
+
+#true or false if it uppercase or not
+phrase = "Giraffe Academy"
+print(phrase.isupper())
+
+#all uppercase
+phrase = "Giraffe Academy"
+print(phrase.upper())
+
+
+#all lowercase
+phrase = "Giraffe Academy"
+print(phrase.lower())
+
+#concatination
+phrase = "Giraffe Academy"
+print(phrase + " is cool")
+
+
+phrase = "Giraffe Academy"
+print(phrase)
+
+#print out quotation mark
+print("great\"game")
+
+
+#add a line
+print("great\ngame")
+
---
+++ variable.py
@@ -0,0 +1,22 @@
+
+#Data types
+    # String - text
+    # Number - whole & decimal numbers
+    # Boolean - true/false
+    
+
+
+character_name = "Tom"
+character_age = "50"
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age +  " years old.")
+
+character_name = "Mike"
+character_age = "25"
+print("He really liked the name " + character_name)
+print("but didn't like being " + character_age + ".")
+
+
+
+
